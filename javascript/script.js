50be57443032d251633d624f6f9fee1866464669
Marto <martin.romao@hotmail.com>
2021-11-07 18:55:48 -0300

    fim2

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,18 @@
-//var arrayNatan = [];
-//var count= 0;
-//var contNatan= document.getElementById("contadorNatan").innerHTML;
-//var erroNatan= document.getElementById("erroNatan").innerHTML;
+var arrayNatan = [];
+var count= 0;
+var contNatan= document.getElementById("contadorNatan").innerHTML;
+var erroNatan= document.getElementById("erroNatan").innerHTML;
 
-/*function ContarNatan(){
-  
+function ContarNatan(){
+    
     if (document.getElementById("inputNatan").value == "natan"){
         count +=1;
     }
     if(document.getElementById("inputNatan").value != "natan"){
         document.getElementById("erroNatan").innerHTML= "digita natan, não " + document.getElementById("inputNatan").value;
     }
-
     console.log(count);
     document.getElementById("contadorNatan").innerHTML= count;
     document.getElementById("inputNatan").value="";
     
-}*/
+}
